5eb3991868648fa851afa398d919568a45f272fc
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-05-29 03:48:08 +0300

    pushing the limits of what's possible

--- src/completion/completion.service.ts
+++ src/completion/completion.service.ts
@@ -42,7 +42,7 @@ export class CompletionService {
     baseurl: 'https://api.openai.com',
     type: 'chatGPT',
     model: 'gpt-3.5-turbo',
-    timeout: 20 * 1000,
+    timeout: 40 * 1000,
   });
 
   async completeResponse(text: string) {
@@ -61,13 +61,58 @@ export class CompletionService {
     const post = `Please parse this resume and return the information in the following JSON format:
     {
       "name": "<name from resume>",
-      "email": "<email from resume>",
+      "email": "<email from resume>", 
       "phone": "<phone number from resume>",
+      "linkedin": "<linkedin profile url>",
+      "github": "<github profile url>",
+      "website": "<personal website url>",
       "position": "<most recent position from experience section>",
       "company": "<most recent company from experience section>",
-      "degree": "<highest degree from education section>", 
+      "experience": [
+        {
+          "role": "<role>",
+          "company": "<company>",
+          "start_date": "<start date>",
+          "end_date": "<end date>",
+          "location": "<location>",
+          "description": "<experience description>"
+        }
+      ],
+      "degree": "<highest degree from education section>",
       "university": "<university from education section>",
-      "skills": ["<list of skills from skills section>"] 
+      "education": [
+        {
+          "degree": "<degree>",
+          "university": "<university>",
+          "start_date": "<start date>",
+          "end_date": "<end date>",
+          "gpa": "<gpa>"
+        }
+      ],
+      "skills": ["<list of skills>"],
+      "languages": ["<list of languages>"],
+      "interests": ["<list of interests>"],
+      "awards": [
+        {
+          "title": "<award title>",
+          "organization": "<organization>",
+          "date": "<date received>"
+        }
+      ],
+      "publications": [
+        {
+          "title": "<publication title>",
+          "publisher": "<publisher>",
+          "date": "<date published>"
+        }
+      ],
+      "certifications": [
+        {
+          "title": "<certification title>",
+          "organization": "<organization>",
+          "date": "<date completed>"
+        }
+      ] 
     }
     `;
 

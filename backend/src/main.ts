ee9bcd2869a4c890282a10c4d7f92646fb320953
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-06-15 12:28:09 +0300

    fixing cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors();
 
   // Create a Swagger document
   const options = new DocumentBuilder()

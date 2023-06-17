4e9348c9201f21d8e07203f8bdb4eedbdc04356c
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-06-17 09:28:42 +0300

    Adding some styles to uploader component

--- frontend/components/ui/file-uploader.tsx
+++ frontend/components/ui/file-uploader.tsx
@@ -4,6 +4,8 @@ import Chip from './upload-chip';
 
 export default function FileUploader() {
   const [files, setFiles] = useState<File[]>([]);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState('');
 
   const { user } = useUser();
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,6 +20,7 @@ export default function FileUploader() {
     formData.append('email', email);
 
     try {
+      setUploading(true);
       const result = await fetch(
         `${process.env.API_URL}/document-ingestion/pdf`,
         {
@@ -27,8 +30,10 @@ export default function FileUploader() {
       );
 
       setFiles((prevFiles) => [...prevFiles, ...uploadedFiles]);
-      console.log(result);
+      setUploading(false);
     } catch (e) {
+      setUploading(false);
+      setError("Error uploading file");
       console.error(e);
     }
   };
@@ -45,7 +50,34 @@ export default function FileUploader() {
         />
       </label>
 
-      <div className="flex flex-wrap w-48">
+      {uploading ? (
+        <div className="mt-4 flex items-center text-gray-500">
+          <svg
+            className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          <span>Uploading...</span>
+        </div>
+      ) : null}
+
+      {error && <div className="mt-4 text-red-500">{error}</div>}
+      <div className="flex flex-wrap w-48 mt-4">
         {files.map((file) => (
           <div key={file.name} className="m-2 p-2 border-2 border-dashed">
             {file.name}

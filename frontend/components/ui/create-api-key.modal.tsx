6c91fbe51ad30f4f6ad9041c3d3c2ececead7faf
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-07-14 22:31:17 +0300

    Adding some nice looking ui

---
+++ frontend/components/ui/create-api-key.modal.tsx
@@ -0,0 +1,61 @@
+// Modal.js
+
+import { useState } from 'react';
+
+interface Props {
+  onSubmit: (name: string) => void;
+}
+
+export default function Modal({ onSubmit }: Props) {
+  const [name, setName] = useState('');
+
+  const handleSubmit = () => {
+    onSubmit(name);
+  };
+
+  return (
+    <div className="z-50 fixed inset-0 overflow-y-auto">
+      <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div
+          className="w-full transform rounded-2xl p-6 text-left align-middle shadow-xl transition-all max-w-md bg-white opacity-100 scale-100"
+          id="headlessui-dialog-panel-:rn:"
+          data-headlessui-state="open"
+        >
+          <h3
+            className="text-lg font-medium leading-6 text-stone-800 mb-1"
+            id="headlessui-dialog-title-:ro:"
+            data-headlessui-state="open"
+          >
+            Create Key
+          </h3>
+          <div className="max-h-[80vh] flex flex-col">
+            <div className="w-full text-stone-500 sm:text-right pb-4">
+              <div className="flex-grow">
+                <div className="relative flex items-center">
+                  <pre
+                    aria-hidden="true"
+                    className="box-border overflow-hidden whitespace-pre-wrap p-2 text-sm leading-5 font-sans"
+                  ></pre>
+                  <textarea
+                    name=""
+                    placeholder="shanur-rahman-secret-key"
+                    className="box-border overflow-hidden whitespace-pre-wrap p-2 text-sm leading-5 font-sans text-stone-600 placeholder:text-stone-400 border-stone-200 border-[.1rem] w-full h-[2.5rem] absolute top-0 resize-none transition-colors transition-transform rounded focus:border-blue-500 focus:ring-blue-500 disabled:bg-stone-50"
+                    tabIndex={0}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4 gap-2 flex flex-row-reverse">
+              <button className="inline-flex items-center justify-center gap-1 py-2 px-3 text-sm font-semibold md:font-medium rounded-lg transition-colors bg-uivory-300 ring-1 ring-inset ring-stone-200 hover:bg-white">
+                Create Key
+              </button>
+              <button className="inline-flex items-center justify-center gap-1 py-2 px-3 text-sm font-semibold md:font-medium rounded-lg transition-colors bg-uivory-300 ring-1 ring-inset ring-stone-200 hover:bg-white">
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/pages/api-key-management.tsx
+++ frontend/pages/api-key-management.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/ui/create-api-key.modal';
 import Header from '@/components/ui/header';
 import { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
@@ -35,117 +36,244 @@ export default function ApiKeys() {
     setNewName('');
   }
 
+  const handleCreate = (name: string) => {
+    console.log(name);
+  };
+
+
   return (
     <div>
       <Header />
-
-      <div className="bg-white shadow rounded-lg p-8">
-        <div className="flex items-center">
-          <GoRadioTower className="w-12 h-12 text-indigo-500" />
-          <h2 className="ml-4 text-2xl font-bold tracking-tight">
-            Ready to deploy?
-          </h2>
-        </div>
-
-        <hr className="my-6 border-gray-300" />
-
-        <ul className="space-y-4">
-          <li className="flex items-center">
-            <GoRocket className="w-6 h-6 text-indigo-500 mr-2" />
-            <span>Increased rate limits to support production traffic</span>
-          </li>
-          <li className="flex items-center">
-            <HiOutlineLightBulb className="w-6 h-6 text-indigo-500 mr-2" />
-            <span>Use Claude in commercial applications</span>
-          </li>
-        </ul>
-
-        <hr className="my-6 border-gray-300" />
-
-        <h3 className="text-lg font-medium mb-4">API Keys</h3>
-
-        {/* API keys content start */}
-        <div className="h-screen overflow-y-auto sm:rounded-md">
-          <ul className="divide-y divide-gray-200">
-            {apiKeys.map((key) => (
-              <li key={key.id} className="px-4 py-4 hover:bg-gray-50 sm:px-6">
-                <div className="flex items-center justify-between">
-                  <div className="truncate text-sm leading-5 font-medium text-gray-900 max-w-xs">
-                    {key.name}
+      <Modal onSubmit={handleCreate} /> 
+      <main className="flex flex-grow min-h-0 w-full overflow-auto">
+        <div className="flex-grow w-full overflow-scroll">
+          <div className="flex flex-col p-6 max-w-4xl mx-auto h-full">
+            <div
+              className="flex rounded-2xl bg-stone-100 border border-stone-200 py-6 px-6 mb-12 items-center"
+              role="alert"
+            >
+              <div className="flex flex-col grow gap-4">
+                <h2 className="flex flex-row text-2xl font-medium text-stone-700">
+                  Ready to deploy?
+                </h2>
+                <div className="flex flex-col lg:flex-row lg:items-end gap-6">
+                  <div className="flex flex-col grow gap-2">
+                    <div className="flex w-full items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        viewBox="0 0 256 256"
+                        className="h-5 w-auto text-stone-700 "
+                        aria-hidden="true"
+                      >
+                        <path d="M207.06,80.67A111.24,111.24,0,0,0,128,48h-.4C66.07,48.21,16,99,16,161.13V184a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V160A111.25,111.25,0,0,0,207.06,80.67ZM224,184H119.71l54.76-75.3a8,8,0,0,0-12.94-9.42L99.92,184H32V161.13c0-3.08.15-6.12.43-9.13H56a8,8,0,0,0,0-16H35.27c10.32-38.86,44-68.24,84.73-71.66V88a8,8,0,0,0,16,0V64.33A96.14,96.14,0,0,1,221,136H200a8,8,0,0,0,0,16h23.67c.21,2.65.33,5.31.33,8Z"></path>
+                      </svg>
+                      <div className="ml-3 flex-1 flex items-center">
+                        <p className="text-normal text-stone-700">
+                          Increased rate limits to support production traffic
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex w-full items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        viewBox="0 0 256 256"
+                        className="h-5 w-auto text-stone-700"
+                        aria-hidden="true"
+                      >
+                        <path d="M103.77,185.94C103.38,187.49,93.63,224,40,224a8,8,0,0,1-8-8c0-53.63,36.51-63.38,38.06-63.77a8,8,0,0,1,3.88,15.53c-.9.25-22.42,6.54-25.56,39.86C81.7,204.48,88,183,88.26,182a8,8,0,0,1,15.51,4Zm93-67.4L192,123.31v58.33A15.91,15.91,0,0,1,187.32,193L153,227.3A15.91,15.91,0,0,1,141.7,232a16.11,16.11,0,0,1-5.1-.83,15.94,15.94,0,0,1-10.78-12.92l-5.37-38.49L76.24,135.55l-38.47-5.37A16,16,0,0,1,28.7,103L63,68.68A15.91,15.91,0,0,1,74.36,64h58.33l4.77-4.77c26.68-26.67,58.83-27.82,71.41-27.07a16,16,0,0,1,15,15C224.6,59.71,223.45,91.86,196.78,118.54ZM40,114.34l37.15,5.18L116.69,80H74.36ZM91.32,128,128,164.68l57.45-57.45a76.46,76.46,0,0,0,22.42-59.16,76.65,76.65,0,0,0-59.11,22.47ZM176,139.31l-39.53,39.53L141.67,216,176,181.64Z"></path>
+                      </svg>
+                      <div className="ml-3 flex-1 flex items-center">
+                        <p className="text-normal text-stone-700">
+                          Use Claude in commercial applications
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="text-sm leading-5 text-gray-500 flex-shrink-0">
-                    {key.key}...
-                  </div>
-                  <div className="text-sm leading-5 text-gray-500 flex-shrink-0 space-x-4">
-                    <button
-                      onClick={() => setDeleteKey(key)}
-                      className="text-gray-500 hover:text-red-500"
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => navigator.clipboard.writeText(key.key)}
-                      className="text-indigo-600 hover:text-indigo-500"
-                    >
-                      Copy
-                    </button>
-                  </div>
-                </div>
-              </li>
-            ))}
-
-            {deleteKey && (
-              <div className="fixed inset-0 flex items-center justify-center z-50">
-                <div className="bg-white w-full max-w-md p-6 rounded-md shadow-md">
-                  <h2 className="text-xl font-semibold mb-4">
-                    Confirm Deletion
-                  </h2>
-                  <p className="text-gray-800 mb-6">
-                    Are you sure you want to delete the key: {deleteKey.name}?
-                  </p>
-                  <div className="flex justify-end">
-                    <button
-                      onClick={() => {
-                        handleDelete(deleteKey.id);
-                        setDeleteKey(null);
-                      }}
-                      className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
-                    >
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => setDeleteKey(null)}
-                      className="px-4 py-2 bg-gray-300 text-gray-800 rounded-md ml-2 hover:bg-gray-400"
-                    >
-                      Cancel
+                  <div className="flex w-full md:w-auto self-justify-end justify-end shrink-0">
+                    <button className="inline-flex items-center justify-center gap-1 py-2 font-semibold md:font-medium rounded-lg transition-colors disabled:opacity-50 disabled:pointer-events-none px-3 bg-ant-primary hover:bg-ant-primary-h disabled:bg-ant-secondary-h text-white w-full md:w-auto ">
+                      Upgrade API Keys
                     </button>
                   </div>
                 </div>
               </div>
-            )}
-          </ul>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6">
-            <div className="flex">
-              <input
-                type="text"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                placeholder="Enter a name"
-                className="form-input flex-1 block w-full rounded-md sm:text-sm sm:leading-5"
-              />
-              <button
-                onClick={handleAddKey}
-                disabled={!newName}
-                className="ml-3 inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700"
-              >
-                <AiOutlinePlus className="-ml-1 mr-2 h-5 w-5" />
-                Add
-              </button>
+            </div>
+            <div className="">
+              <div className="flex flex-row justify-between items-center">
+                <div className="py-4 font-medium text-stone-800 text-2xl">
+                  API Keys
+                </div>
+                <div className="my-4 text-right">
+                  <button className="inline-flex items-center justify-center gap-1 py-2 font-semibold md:font-medium rounded-lg transition-colors disabled:opacity-50 disabled:pointer-events-none px-3 bg-uivory-300 ring-1 ring-inset ring-stone-200 hover:bg-white">
+                    Create Key
+                  </button>
+                </div>
+              </div>
+              <div className="overflow-x-auto sm:-mx-0">
+                <table className="min-w-full divide-y divide-stone-300 border-b border-stone-200 whitespace-nowrap">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="text-left text-[.685rem] font-medium uppercase tracking-wider text-stone-500  py-3 pr-3 sm:pl-0"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-left text-[.685rem] font-medium uppercase tracking-wider text-stone-500  py-3 pr-3 sm:pl-0"
+                      >
+                        Key
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-left text-[.685rem] font-medium uppercase tracking-wider text-stone-500  py-3 sm:table-cell"
+                      >
+                        Created at
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-left text-[.685rem] font-medium uppercase tracking-wider text-stone-500 px-3 py-3 sm:table-cell"
+                      >
+                        Updated at
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-right text-[.685rem] font-medium uppercase tracking-wider text-stone-500 pl-3 py-3 sm:table-cell"
+                      >
+                        Enabled
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-stone-200">
+                    <tr>
+                      <td className="whitespace-normal overflow-x-auto overflow-hidden sm:overflow-visible w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        CLAUDE_SECRET
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0"></td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        Apr 7, 2023
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3 text-sm text-stone-500 lg:table-cell"></td>
+                      <td className="flex h-full items-center py-3 pr-6 sm:pr-0">
+                        <div className="grow w-0 h-0"></div>
+                        <div className="flex items-center">
+                          <button
+                            className="bg-ant-primary relative inline-flex h-6 w-11 items-center rounded-full"
+                            id="headlessui-switch-:rf:"
+                            role="switch"
+                            type="button"
+                            tabIndex={0}
+                            aria-checked="true"
+                            data-headlessui-state="checked"
+                          >
+                            <span className="sr-only">Enable key</span>
+                            <span className="translate-x-6 inline-block h-4 w-4 transform rounded-full bg-white transition"></span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="whitespace-normal overflow-x-auto overflow-hidden sm:overflow-visible w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        yckey
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0"></td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        Apr 6, 2023
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        May 19, 2023
+                      </td>
+                      <td className="flex h-full items-center py-3 pr-6 sm:pr-0">
+                        <div className="grow w-0 h-0"></div>
+                        <div className="flex items-center">
+                          <button
+                            className="bg-stone-300 relative inline-flex h-6 w-11 items-center rounded-full"
+                            id="headlessui-switch-:rg:"
+                            role="switch"
+                            type="button"
+                            tabIndex={0}
+                            aria-checked="false"
+                            data-headlessui-state=""
+                          >
+                            <span className="sr-only">Enable key</span>
+                            <span className="translate-x-1 inline-block h-4 w-4 transform rounded-full bg-white transition"></span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="whitespace-normal overflow-x-auto overflow-hidden sm:overflow-visible w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        shanur-secret
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        sk-ant-api03-iTv...UQAA
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        May 26, 2023
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3 text-sm text-stone-500 lg:table-cell"></td>
+                      <td className="flex h-full items-center py-3 pr-6 sm:pr-0">
+                        <div className="grow w-0 h-0"></div>
+                        <div className="flex items-center">
+                          <button
+                            className="bg-ant-primary relative inline-flex h-6 w-11 items-center rounded-full"
+                            id="headlessui-switch-:rh:"
+                            role="switch"
+                            type="button"
+                            tabIndex={0}
+                            aria-checked="true"
+                            data-headlessui-state="checked"
+                          >
+                            <span className="sr-only">Enable key</span>
+                            <span className="translate-x-6 inline-block h-4 w-4 transform rounded-full bg-white transition"></span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="whitespace-normal overflow-x-auto overflow-hidden sm:overflow-visible w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        llmskey
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal w-full max-w-0 py-4 pl-6 pr-3 text-sm sm:w-auto sm:max-w-none sm:pl-0">
+                        sk-ant-api03-wY-...OAAA
+                      </td>
+                      <td className="overflow-x-auto overflow-ellipsis whitespace-nowrap sm:overflow-visible sm:whitespace-normal px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        May 19, 2023
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3 text-sm text-stone-500 lg:table-cell">
+                        May 26, 2023
+                      </td>
+                      <td className="flex h-full items-center py-3 pr-6 sm:pr-0">
+                        <div className="grow w-0 h-0"></div>
+                        <div className="flex items-center">
+                          <button
+                            className="bg-stone-300 relative inline-flex h-6 w-11 items-center rounded-full"
+                            id="headlessui-switch-:ri:"
+                            role="switch"
+                            type="button"
+                            tabIndex={0}
+                            aria-checked="false"
+                            data-headlessui-state=""
+                          >
+                            <span className="sr-only">Enable key</span>
+                            <span className="translate-x-1 inline-block h-4 w-4 transform rounded-full bg-white transition"></span>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-        {/* API keys content end */}
-      </div>
+      </main>
     </div>
   );
 }
